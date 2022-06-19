a57e34c64d0feef5e0948980191abdf4f560676a
Dmytro Ostrovskyi <ostrovskyi.d@gmail.com>
2022-06-20 01:00:16 +0300

    changed pull to fetch

--- apply_configs.ps1
+++ apply_configs.ps1
@@ -1,4 +1,4 @@
-git pull
+git fetch
 New-Item -Path '~\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\hrow_vim_nav.ahk' -ItemType SymbolicLink -Value .\hrow_vim_nav.ahk -Force;
 New-Item -Path '~\.ideavimrc' -ItemType SymbolicLink -Value .\.ideavimrc -Force;
 New-Item -Path '~\.atamanrc.config' -ItemType SymbolicLink -Value .\.atamanrc.config -Force;
