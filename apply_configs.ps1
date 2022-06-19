3b71265ee4e58f1b607815ead0b0ed720e07c42e
Dima Ostrovskyi <ostrovskyi.d@gmail.com>
2022-06-20 00:36:11 +0300

    added git pull to script

--- apply_configs.ps1
+++ apply_configs.ps1
@@ -1,3 +1,4 @@
+git pull
 New-Item -Path '~\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\hrow_vim_nav.ahk' -ItemType SymbolicLink -Value .\hrow_vim_nav.ahk -Force;
 New-Item -Path '~\.ideavimrc' -ItemType SymbolicLink -Value .\.ideavimrc -Force;
 New-Item -Path '~\.atamanrc.config' -ItemType SymbolicLink -Value .\.atamanrc.config -Force;
