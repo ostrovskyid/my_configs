ec7578614ff6361792e362a31914c5d2cf58e540
Dmytro Ostrovskyi <ostrovskyi.d@gmail.com>
2023-06-24 15:26:05 +0200

    copy configs script

--- apply_configs_files_copy.ps1
+++ apply_configs_files_copy.ps1
@@ -1,4 +1,4 @@
 git fetch
 New-Item -Path '~\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup\hrow_vim_nav.ahk' -ItemType SymbolicLink -Value .\hrow_vim_nav.ahk -Force;
-New-Item -Path '~\.ideavimrc' -Value .\.ideavimrc -Force;
-New-Item -Path '~\.atamanrc.config' -Value .\.atamanrc.config -Force;
+Copy-Item -Path ".\.ideavimrc" -Destination "~\.ideavimrc" -Force
+Copy-Item -Path ".\.atamanrc.config" -Destination "~\.atamanrc.config" -Force
